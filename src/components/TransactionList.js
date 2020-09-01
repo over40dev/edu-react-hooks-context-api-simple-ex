8b153c38f14659d7ff3ff753062651fdf7ece511
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-09-01 18:19:08 -0230

    react-hooks-context-simple-ex (useContext)

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+        {transactions.map(t => (
+          <li className="minus">
+            {t.text} <span>-$400</span><button className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   )
