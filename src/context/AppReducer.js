095d3a55175dc43bfb6923eb235f58861b09fadd
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-09-01 18:04:22 -0230

    react-hooks-context-simple-ex (context / reducer)

---
+++ src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// Reducers - specify state changes in response to actions to our state/context
+
+export default (state, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
